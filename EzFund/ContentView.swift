e41ee417ae7d5317cf9302474e59f97c61f09de8
Zrzzzz <1160026659@qq.com>
2021-01-22 10:31:41 +0800

    Initial Commit

---
+++ EzFund/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  EzFund
+//
+//  Created by Zr埋 on 2021/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
